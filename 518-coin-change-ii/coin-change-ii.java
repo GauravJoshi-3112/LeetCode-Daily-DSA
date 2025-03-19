38e976d1ea2263a6eed86109e987d292f04f5185
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-03-20 00:40:19 +0530

    Time: 12 ms (28.29%) | Memory: 51.5 MB (9.31%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    // This is a typial Unbounded KnapSack Problem 
+    // How ?
+    // As we have the option again to Select the Coin Once Chosen 
+    int dp [][];
+    public int change(int amount, int[] coins) {
+        if (amount == 0) return 1;
+        dp = new int [amount+1][coins.length+1];
+        
+
+        for(int j=0; j<coins.length+1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i=0; i<amount+1; i++) {
+            dp[i][0] = 0; // This Updation is for j = 0
+        }
+
+        // Initialization is DONE
+
+        for(int i=1; i<amount+1; i++) {
+            for(int j=1; j<coins.length+1; j++) {
+                if (i < coins[j-1]) {
+                    dp[i][j] = dp[i][j-1];
+                } else {
+                    dp[i][j] =  (coins[j-1] == i) ? dp[i][j-1] + dp[i-coins[j-1]][j] + 1 : dp[i][j-1] + dp[i-coins[j-1]][j];
+                }
+            }
+        }
+        return dp[amount][coins.length];
+    }
+}
