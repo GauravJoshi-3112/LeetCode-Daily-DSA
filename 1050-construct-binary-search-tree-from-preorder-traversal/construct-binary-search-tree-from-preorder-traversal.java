3413e1a0e710ed19573fcaa481255f7464be879b
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-04-09 22:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (7.04%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,25 +14,20 @@
  * }
  */
 class Solution {
-    public TreeNode bstFromPreorderAndInorder(int [] preorder,int i1, int j1, int [] inorder,int i2, int j2) {
-        if (j1 < i1 || j2 < i2) return null;
-        TreeNode root = new TreeNode(preorder[i1]);
-        int i = i2;
-        while (i <= j2) {
-            if (inorder[i] == preorder[i1]) break;  
-            i++;
+    public TreeNode bstFromPreorder(int [] preorder, int i, int j) {
+        if (i >= j) return null;
+        TreeNode root = new TreeNode(preorder[i]);
+        int jN = i+1;
+        while (jN < j) {
+            if (preorder[jN] > root.val) break;
+            jN++;
         }
 
-        root.left = bstFromPreorderAndInorder(preorder, i1+1, i1+i-i2, inorder, i2, i-1);
-        root.right = bstFromPreorderAndInorder(preorder,i1+i-i2+1, j1, inorder, i+1, j2);
+        root.left = bstFromPreorder(preorder, i+1, jN);
+        root.right = bstFromPreorder(preorder, jN, j);
         return root;
     }
-
     public TreeNode bstFromPreorder(int[] preorder) {
-        int [] inorder = preorder.clone();
-        Arrays.sort(inorder);
-        
-        // Got both Inorder and PreOrder
-        return bstFromPreorderAndInorder(preorder,0,preorder.length-1, inorder, 0, inorder.length-1);
+        return bstFromPreorder(preorder,0,preorder.length);
     }
 }
