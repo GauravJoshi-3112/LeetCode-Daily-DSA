e5f3de725eb01c1226ab0b7defaebee896fa7574
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-04-28 20:28:15 +0530

    Time: 22 ms (44.95%) | Memory: 51 MB (42.78%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,27 +1,11 @@
 class Solution {
-    int [][] dp;
-    // public int LCS(String text1, String text2) {
-    //     if (text1.length()==0 || text2.length()==0) return 0;
-    //     if (text1.charAt(0) == text2.charAt(0)) return 1+LCS(text1.substring(1),text2.substring(1));
-    //     else {
-    //         int max1 = dp[text1.length()][text2.length()-1] != -1 ? dp[text1.length()][text2.length()-1] : LCS(text1,text2.substring(1));
-    //         dp[text1.length()][text2.length()-1] = max1;
-    //         int max2 = dp[text1.length()-1][text2.length()] != -1 ? dp[text1.length()-1][text2.length()] : LCS(text1.substring(1),text2);
-    //         dp[text1.length()-1][text2.length()] = max2;
-    //         dp[text1.length()][text2.length()] = Math.max(max1,max2);
-    //     }
-    //     return dp[text1.length()][text2.length()];
-    // }
+    int dp [][];
     public int longestCommonSubsequence(String text1, String text2) {
         dp = new int[text1.length()+1][text2.length()+1];
-        for(int i=0; i<text1.length()+1; i++) {
-            dp[i][0] = 0;
-        }
-        for(int j=0; j<text2.length()+1; j++) {
-            dp[0][j] = 0;
-        }
-        for(int i=1; i<text1.length()+1; i++) {
-            for(int j=1; j<text2.length()+1; j++) {
+        // No Need to Initiaze as Common Subsequence length of empty String is 0 (Default for int Array Initialization)
+
+        for(int i=1; i<=text1.length(); i++) {
+            for(int j=1; j<=text2.length(); j++) {
                 if (text1.charAt(i-1) == text2.charAt(j-1)) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
