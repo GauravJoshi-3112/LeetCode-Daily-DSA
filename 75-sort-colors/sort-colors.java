0f1e86374a26ed2cf9cd558578c5d55eba2624e7
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-05-24 20:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (96.91%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,8 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // Typical Dutch National Flag Algo Problem
-        int i = 0, j = 0, k = nums.length-1;
-        while (j <= k) {
+        int i = 0, j = 0, k = nums.length - 1;
+        while(j <= k) {
             if (nums[j] == 2) {
                 nums[j] = nums[k];
                 nums[k] = 2;
@@ -10,7 +9,7 @@ class Solution {
             } else if (nums[j] == 0) {
                 nums[j] = nums[i];
                 nums[i] = 0;
-                i++; 
+                i++;
                 j++;
             } else {
                 j++;
