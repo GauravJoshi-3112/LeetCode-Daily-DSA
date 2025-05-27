1348734425a90bcd1040b30596165352e223a0bb
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-05-27 21:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (82.55%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,10 +1,16 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        for(int i=1; i<=n; i++) {
-            if (i%m == 0) num2 += i;
-            else num1 += i;
-        }
+        // This Problem can also be achieved in O(1) complexity 
+        // Identify the sum of n 
+        int sumN = (n*(n+1))/2;
+
+        // Found the Sum of n Natural Number such that divisible by m
+        int k = n/m;
+        int num2 = (m)*(k*(k+1))/2;
+
+        // Found num1 
+        int num1 = sumN - num2;
+
         return num1 - num2;
     }
 }
